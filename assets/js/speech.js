5201fc50f5d4a85134f1b0f51748a04e8cc17c3a
Mohamed M ali <101191058+mohamed20039@users.noreply.github.com>
2023-04-23 11:54:04 -0700

    Add files via upload

---
+++ assets/js/speech.js
@@ -0,0 +1,13 @@
+
+
+
+
+
+// console.log(card)
+
+
+
+// for (let i = 0; i < elements.length; i++) {
+//   const utterance = new SpeechSynthesisUtterance(elements[i].textContent);
+//   speechSynthesis.speak(utterance);
+// }
